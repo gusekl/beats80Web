4a8296c50418dec69fa8f8918ebc02052505c4d8
Gustaf Eklund <gustafeklund0@gmail.com>
2024-09-02 17:34:35 +0200

    Initial commit, create-react-app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <div>
+      <div className='header'>
         <Navbar modules={["1","hej"]}/>
       </div>
     </div>
