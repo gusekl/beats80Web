fe5702ac92ced03f64355da033a829bb8740e4b1
Gustaf Eklund <gustafeklund0@gmail.com>
2024-09-03 13:38:04 +0200

    Branch-test

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-        <img src={require('./images/Beatslogga.png')} alt='beats logo' height={"128px"}/>
-        <h1 className='primary-color'>Här kommer snart Beats 80s helt nya hemsida!</h1>
-        <h2 className='primary-color'>Tills dess kan ni följa allt vi gör på vår <a href='https://www.facebook.com/beats.2024'>Facebook</a>!</h2>
     </div>
   );
 }
